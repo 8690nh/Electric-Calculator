2ccec1f658556904f3e2406f3595fec902c8b41a
8690nh <161710362+8690nh@users.noreply.github.com>
2024-03-17 17:19:51 +0700

    Add new project food

--- javainput.js
+++ javainput.js
@@ -1,22 +1,3 @@
-/* chỉ số điện mới và cũ, đơn giá điện theo từng khoảng
-và các khoản phí cố định nếu có. Sau đó, chương trình sẽ
-tính và hiển thị tổng số tiền cần thanh toán cho hóa đơn
-
-  A = P x T
-  A là điện năng đã tiêu thụ kWh
-  P là công suất tiêu thụ KW
-  T là thời gian sử dụng h
-  0.41 for daile usage
- */
-
-// let ue = prompt("Enter your daily kWh usage"); // used electric
-// let me = prompt("Enter your monthly kWh usage"); // monthly electric usage
-// let cd = new Date().getMonth(); // curren date
-// let x, y, z;
-// z = cd - ue;
-// x = ue * 24;
-// y = x * z;
-
 let x = prompt("Tổng điện năng tiêu thụ (kWh)");
 if (x <= 50) {
     if (x > 51 && x <= 100) {
